e58bda7a21b4ab6e6d434f64663787d8cc187220
Roger Kondrat <misterhtmlcss@gmail.com>
2021-03-06 22:45:00 -0700

    destructured to clean up repetative code

--- components/Snippet.js
+++ components/Snippet.js
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import Code from './Code';
 
 export default function Snippet({ snippet, snippetDeleted }) {
+  const { data, id } = snippet
+  const { name, language, description, code } = data
   const deleteSnippet = async () => {
     try {
       await fetch('/api/deleteSnippet', {
@@ -20,14 +22,14 @@ export default function Snippet({ snippet, snippetDeleted }) {
   return (
     <div className="bg-gray-100 p-4 rounded-md my-2 shadow-lg">
       <div className="flex items-center justify-between mb-2">
-        <h2 className="text-xl text-gray-800 font-bold">{snippet.data.name}</h2>
+        <h2 className="text-xl text-gray-800 font-bold">{name}</h2>
         <span className="font-bold text-xs text-red-800 px-2 py-1 rounded-lg ">
-          {snippet.data.language}
+          {language}
         </span>
       </div>
-      <p className="text-gray-900 mb-4">{snippet.data.description}</p>
-      <Code code={snippet.data.code} />
-      <Link href={`/edit/${snippet.id}`}>
+      <p className="text-gray-900 mb-4">{description}</p>
+      <Code code={code} />
+      <Link href={`/edit/${id}`}>
         <a className="text-gray-800 mr-2">Edit</a>
       </Link>
       <button
