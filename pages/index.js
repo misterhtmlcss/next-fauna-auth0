96a3dd2c6248fdf2c9063371b472bef432a9038d
Roger Kondrat <misterhtmlcss@gmail.com>
2021-03-06 22:45:52 -0700

    Dealt with html meta favicon and title

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Nextjs App</title>
+        <title>MHC Nextjs App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
