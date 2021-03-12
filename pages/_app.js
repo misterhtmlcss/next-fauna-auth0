d9565c4249a401df34557d1f02ec6eca291ebcc9
Roger Kondrat <misterhtmlcss@gmail.com>
2021-03-12 13:28:57 -0700

    updated app with Auth0 UserProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,18 @@
+import { UserProvider } from '@auth0/nextjs-auth0';
 import NavBar from '../components/NavBar';
-// import { UserProvider } from '@auth0/nextjs-auth0';
 
 import '../styles/app.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    // <UserProvider>
-    <div className="bg-blue-600 w-full p-10 min-h-screen">
-      <div className="max-w-2xl mx-auto">
-        <NavBar>
+    <UserProvider>
+      <div className="bg-blue-600 w-full p-10 min-h-screen">
+        <div className="max-w-2xl mx-auto">
+          <NavBar />
           <Component {...pageProps} />
-        </NavBar>
+        </div>
       </div>
-    </div>
-    // </UserProvider>
+    </UserProvider>
   );
 }
 
