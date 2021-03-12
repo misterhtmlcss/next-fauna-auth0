f7f2cec1df401a42963d7e8d07e6278288db350b
Roger Kondrat <misterhtmlcss@gmail.com>
2021-03-12 13:32:09 -0700

    undid some commented out material due to testing issues

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,9 @@
 import Link from 'next/link';
-// import { useUser } from '@auth0/nextjs-auth0';
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Navbar() {
-  // const { user, error, isLoading } = useUser()
-
+  const { user, error, isLoading } = useUser();
+  if (error) return <div>{error.message}</div>;
   return (
     <nav>
       <Link href="/">
@@ -21,26 +21,23 @@ export default function Navbar() {
         <Link href="/snippets/javascript">
           <a className="hover:underline text-gray-100">JavaScript</a>
         </Link>
-        {/* { */}
-        {/* !isLoading && !user ?
-            ( */}
-        <Link href="/api/auth/login">
-          <a className="hover:underline text-green-200">Login</a>
-        </Link>
-        {/* )
-            :
-            user &&
-              ( */}
-        <>
-          <Link href="/api/mysnippets">
-            <a className="hover:underline text-gray-100">MySnippets</a>
+        {!isLoading && !user ? (
+          <Link href="/api/auth/login">
+            <a className="hover:underline text-green-200">Login</a>
           </Link>
-          <Link href="/api/auth/logout">
-            <a className="hover:underline text-green-200">Logout</a>
-          </Link>
-        </>
-        {/* )
-        } */}
+        ) : (
+          !isLoading &&
+          user && (
+            <>
+              <Link href="/api/mysnippets">
+                <a className="hover:underline text-gray-100">MySnippets</a>
+              </Link>
+              <Link href="/api/auth/logout">
+                <a className="hover:underline text-green-200">Logout</a>
+              </Link>
+            </>
+          )
+        )}
       </div>
     </nav>
   );
